d12f8befef7d42197a24d65b45edbdfe4df7e6d6
Tigerbots2509-Member <24500962+Tigerbots2509-Member@users.noreply.github.com>
2020-01-09 17:33:19 -0600

    Added Vision subsystem and methods

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,26 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+    /* Vision Constants */
+
+    // Network
+    public static String kLimelightIP = "10.25.9.11";
+    public static String kLimelightNetworkID = "limelight";
+
+    // Settings
+    public static int kLimelightLED = 3; // Sets LED. 0 = Set by Pipline, 1 = Force off, 2 = Force blink, 3 = Force on
+    public static int kLimelightMode = 0; // Sets camera mode. 0 = Vision processor, 1 = Driver Camera
+    public static int kLimelightStream = 0; // Sets streaming mode. 0 = Side-by-Side, 1 = PiP main, 2 = PiP secondary
+    public static int kLimelightStartingPipeline = 2; // The default pipeline to stream
+    
+    // Table IDs (for getting values from the Network Table)
+    public static String kLimelightLatencyID = "tl"; // Pipeline latency in milliseconds
+    public static String kLimelightTargetID = "tv"; // Whether or not a valid target is found (0 or 1)
+    public static String kLimelightTargetXID = "tx"; // Horizontal offset from crosshair to target (+/- 27 degrees)
+    public static String kLimelightTargetYID = "ty"; // Vertical offset from crosshair to target (+/- 20.5 degrees)
+    public static String kLimelightTargetAreaID = "ta"; // Target area (0-100 % of image)
+    public static String kLimelightTargetSkewID = "ts"; // Target skew/rotation (-90 to 0 degrees)
+    public static String kLimelightTargetVertID = "tvert"; // Vertical sidelength of bounding box (0-320 pixels)
+    public static String kLimelightTargetHorID = "thor"; // Horizontal sidelength of bounding box (0-320 pixels)
 }
