e7d6d0852b7c3f0a10be79c4b815cb6f40cebf85
Tigerbots2509-Member <24500962+Tigerbots2509-Member@users.noreply.github.com>
2020-01-18 10:32:01 -0600

    Mark 1 of radius turning untested

--- src/main/java/frc/robot/VariableVault.java
+++ src/main/java/frc/robot/VariableVault.java
@@ -7,15 +7,23 @@
 
 package frc.robot;
 
+// import com.revrobotics.ColorMatch;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj.util.Color;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
-public final class Constants {
+public final class VariableVault {
 
     /* ID NUMBERS */
     // Joystick
@@ -25,24 +33,27 @@ public final class Constants {
     public static int kLeftRearID = 1;
     public static int kRightFrontID = 2;
     public static int kRightRearID = 3;
+    // Colorwheel
+    public final int kcolorWheelMotorID = 4;
+    public final I2C.Port iPort = Port.kOnboard;
+    // Colors
+    // public final Color kBlue = ColorMatch.makeColor(0.143, 0.427, 0.429); 
+    // public final Color kYellow = ColorMatch.makeColor(0.361, 0.524, 0.113); // when green is under our sensor, yellow is under theirs
+    // public final Color kGreen = ColorMatch.makeColor(0.197, 0.561, 0.240); // when yellow is under our sensor, green is under theirs
+    // public final Color kRed = ColorMatch.makeColor(0.561, 0.232, 0.114); // when blue is under our sensor, red is under theirs
 
-    //RobotThings
-    public static double kWheelGap = 22;
-    public static double kWheelDiameter = 7;
 
-    //EncoderTicks
-    public static double TicksPerRevolution = 256;
     /* Vision Constants */
 
     // Network
-    // public static String kLimelightIP = "10.25.9.1";
+    public static String kLimelightIP = "10.25.9.11";
     public static String kLimelightNetworkID = "limelight";
 
     // Settings
-    public static int kLimelightLED = 0; // Sets LED. 0 = Set by Pipline, 1 = Force off, 2 = Force blink, 3 = Force on
+    public static int kLimelightLED = 3; // Sets LED. 0 = Set by Pipline, 1 = Force off, 2 = Force blink, 3 = Force on
     public static int kLimelightMode = 0; // Sets camera mode. 0 = Vision processor, 1 = Driver Camera
     public static int kLimelightStream = 0; // Sets streaming mode. 0 = Side-by-Side, 1 = PiP main, 2 = PiP secondary
-    public static int kLimelightStartingPipeline = 0; // The default pipeline to stream
+    public static int kLimelightStartingPipeline = 2; // The default pipeline to stream
     
     // Table IDs (for getting values from the Network Table)
     public static String kLimelightLatencyID = "tl"; // Pipeline latency in milliseconds
@@ -53,4 +64,6 @@ public final class Constants {
     public static String kLimelightTargetSkewID = "ts"; // Target skew/rotation (-90 to 0 degrees)
     public static String kLimelightTargetVertID = "tvert"; // Vertical sidelength of bounding box (0-320 pixels)
     public static String kLimelightTargetHorID = "thor"; // Horizontal sidelength of bounding box (0-320 pixels)
+
+
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -8,6 +8,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
@@ -15,11 +16,10 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class Drivetrain extends SubsystemBase {
   
-  public final WPI_TalonSRX LeftFront = new WPI_TalonSRX(Constants.kLeftFrontID);
+  public static WPI_TalonSRX LeftFront = new WPI_TalonSRX(Constants.kLeftFrontID);
   public final WPI_VictorSPX LeftRear = new WPI_VictorSPX(Constants.kLeftRearID);
   public final WPI_TalonSRX RightFront = new WPI_TalonSRX(Constants.kRightFrontID);
   public final WPI_VictorSPX RightRear = new WPI_VictorSPX(Constants.kRightRearID);
@@ -71,6 +71,51 @@ public class Drivetrain extends SubsystemBase {
 		else {
 			Drive.arcadeDrive(Speed, stick.getRawAxis(0) * -TurnSpeedMulti);
     }
+  }
+
+  public void RadiusTurning(double Angle, double Radius, double Speed, String Direction){
+    double ShortMove = Math.PI*Radius*2;
+    double LongMove = Math.PI*(Radius+22)*2;
+
+    double LongSpeed = (Speed*LongMove)/ShortMove;
+    double ShortSpeed = Speed;
+
+    double CircumferencePercent = (Angle*100)/360;
+
+    double ShortAngleMove = ShortMove * CircumferencePercent; //inches
+    double LongAngleMove = LongMove * CircumferencePercent; //inches
+
+    double RightCurrentInches = RightFront.getSelectedSensorPosition() / 11.646951774340309372156505914468;
+    double LeftCurrentInches = LeftFront.getSelectedSensorPosition() / 11.646951774340309372156505914468; // ticks per inch
+
+    if(Direction == "Right"){
+        if(RightCurrentInches < ShortAngleMove){
+          Right.set(ShortSpeed);
+        }else{
+          Right.set(0);
+        }
+        if(LeftCurrentInches < LongAngleMove){
+          Left.set(LongSpeed);
+        }else{
+          Left.set(0);
+        }
+    }else if(Direction == "Left"){
+      if(LeftCurrentInches < ShortAngleMove){
+        Left.set(ShortSpeed);
+      }else{
+        Left.set(0);
+      }
+      if(RightCurrentInches < LongAngleMove){
+        Right.set(LongSpeed);
+      }else{
+        Right.set(0);
+      }
+    }else{
+      //User did something wrong
+      Drive.arcadeDrive(0, 0);
+    }
+
+
   }
 
   @Override
