99fa8948eab91ae50c8aa5775c96359eca0082d0
Tigerbots2509-Member <24500962+Tigerbots2509-Member@users.noreply.github.com>
2020-01-23 18:00:36 -0600

    Added conveyor motor

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -10,9 +10,12 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.VictorSP;
 
 public class Intake extends SubsystemBase {
   private static AnalogInput LightSensor = new AnalogInput(0);
+  private static VictorSP ConveyorMotor = new VictorSP(0);
+
   /**
    * Creates a new Intake.
    */
@@ -22,11 +25,32 @@ public class Intake extends SubsystemBase {
 
   @Override
   public void periodic() {
+    if(GetSensorValue() == true) {
+      ConveyorForward();
+    }else {
+      StopMotor();
+    }
+  }
+
+  private boolean GetSensorValue(){
     if(LightSensor.getVoltage() > 0.5) {
       SmartDashboard.putBoolean("Detecting a Thing", true);
+      return true;
     } else {
       SmartDashboard.putBoolean("Detecting a Thing", false);
+      return false;
     }
-    // This method will be called once per scheduler run
+  } 
+
+  public void ConveyorForward(){
+    ConveyorMotor.set(1);
+  }
+
+  public void ConveyorReverse(){
+    ConveyorMotor.set(-1);
+  }
+
+  public void StopMotor(){
+    ConveyorMotor.set(0);
   }
 }
