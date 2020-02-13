b5bcdb8af13e809ee49bc88e7fd22ad0fa6c42c1
Tigerbots2509-Member <24500962+Tigerbots2509-Member@users.noreply.github.com>
2020-02-13 14:51:08 -0600

    Added shooter motors? For some reason? ~Cecilia

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,12 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -22,6 +26,11 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   public static RobotContainer RobotContainer;
+
+  public static WPI_TalonSRX ShooterMain = new WPI_TalonSRX(4);
+  public static WPI_TalonSRX ShooterSlave = new WPI_TalonSRX(5);
+
+  public static SpeedControllerGroup Shooters = new SpeedControllerGroup(ShooterMain, ShooterSlave);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -87,9 +96,12 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
+    
+    // if (m_autonomousCommand != null) {
+    //   m_autonomousCommand.cancel();
+    // }
+
+    Shooters.set(0.8);
   }
 
   /**
@@ -97,6 +109,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
+    Shooters.set(0.8);
+    SmartDashboard.putNumber("Velocity", ShooterMain.getSelectedSensorVelocity());
   }
 
   @Override
